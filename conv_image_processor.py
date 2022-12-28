f60254895a8b1a842bb117d5412dec8afd05dfa2
Joe-Nunn <Joe.Nunn@Hotmail.com>
2022-12-28 19:23:49 +0000

    Split off convolutional layers to new class

---
+++ conv_image_processor.py
@@ -0,0 +1,52 @@
+"""
+Class to create flattened feature maps of jigsaw pieces and sections of the overall board
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+LAYERS = 3
+
+INPUT_IMAGE_SIZE = 256  # Height and width same
+
+FINAL_OUT_CHANNELS = 128
+
+FLATTENED_TENSOR_SIZE = ((INPUT_IMAGE_SIZE / (2 ^ LAYERS)) ^ 2) * FINAL_OUT_CHANNELS
+
+
+class ConvImageProcessor(nn.Module):
+    """
+    Processes images to create flattened feature maps of jigsaw pieces and sections of the overall board
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # Half size, will be applied after every conv layer. E.g. 128x128 becomes 64x64
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # maintains size and ratio. E.g. input of 256x256 stays 256x256
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=FINAL_OUT_CHANNELS, kernel_size=3, stride=1, padding=1)
+
+        def forward(self, image_tensor):
+            """
+            Creates feature map of an image and returns it flattened
+
+            """
+            # Convolutional layers
+            image_tensor = self.conv1(image_tensor)
+            image_tensor = F.relu(image_tensor)
+            image_tensor = self.pool(image_tensor)
+            image_tensor = self.conv2(image_tensor)
+            image_tensor = F.relu(image_tensor)
+            image_tensor = self.pool(image_tensor)
+            image_tensor = self.conv3(image_tensor)
+            image_tensor = self.relu(image_tensor)
+            image_tensor = self.pool(image_tensor)
+
+            # Flatten
+            image_tensor = image_tensor.reshape(image_tensor.shape[0], -1)
+
+            return image_tensor
--- neural_network.py
+++ neural_network.py
@@ -4,7 +4,7 @@ Contains class NeuralNetwork which defines a neural network to predict image lab
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import conv_image_processor
 
 
 class NeuralNetwork(nn.Module):
@@ -14,38 +14,16 @@ class NeuralNetwork(nn.Module):
     """
     def __init__(self):
         super().__init__()
-
-        # Half size, will be applied after every conv layer
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # maintains size of 256x512
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        # maintains size of 128x256
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-
-        # Original image of size 256x512 is halved twice to 64 * 128 with 64 channels
-        self.fc1 = nn.Linear(64 * 128 * 64, 1000)
+        # Comparing two flattened tensors representing the jigsaw piece and section of the board
+        self.fc1 = nn.Linear(conv_image_processor.FLATTENED_TENSOR_SIZE * 2, 1000)
         self.fc2 = nn.Linear(1000, 1000)
         self.fc3 = nn.Linear(1000, 1)
         self.dropout = nn.Dropout()
 
         def forward(self, batch):
             """
-            Calculates prediction of if the two images combined - the jigsaw piece and the sample of the base are the
-            same location
+            Calculates prediction that jigsaw piece and the sample of the base are the same location
             """
-
-            # Convolutional layers
-            batch = self.conv1(batch)
-            batch = F.relu(batch)
-            batch = self.pool(batch)
-            batch = self.conv2(batch)
-            batch = F.relu(batch)
-            batch = self.pool(batch)
-
-            # Flatten
-            batch = batch.reshape(batch.shape[0], -1)
-
             # Fully connected layers
             batch = self.fc1(batch)
             batch = self.dropout(batch)
